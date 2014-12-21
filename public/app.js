d49eb31a6d262e16b34309a48b25e11405e299c0
Vlad <vladislafus@gmail.com>
2014-12-22 01:55:41 +0200

    Make file DataController

--- public/app.js
+++ public/app.js
@@ -106,32 +106,5 @@ app.controller("MainCtrl", ['$scope', '$http', 'renderCharts', 'Patient', 'Retri
 
     }]);
 
-app.controller('DataCtrl', ['$scope', '$controller', 'renderCharts', 'Patient', 'Data', 'RetrieveData',
-    function ($scope, $controller, renderCharts, Patient, Data, RetrieveData) {
-        $scope.showTempForm = false;
-        $scope.toggleTemperatureForm = function () {
-            $scope.showTempForm = !$scope.showTempForm;
-        };
 
-        $scope.addTemp = function (name, id, date, temperature) {
-            Data.addTemperatureById(id, date, temperature);
-            setTimeout(function () {
-                $scope.$apply(function () {
-                    Patient.getTemperatureById(id)
-                            .then(function (result) {
-                                var obj = RetrieveData.retrieveTemperature(result.data);
-                                renderCharts.renderTempChart(name, obj.dates, obj.temperature);
-                                $scope.tempdates = obj.dates;
-                                $scope.tempvalues = obj.temperature;
-                                $scope.showTempForm = false;
-
-                            });
-
-                });
-            }, 2000);
-            $scope.addtemp = null;
-            $scope.tempdate = null;
-        };
-
-    }]);
 
